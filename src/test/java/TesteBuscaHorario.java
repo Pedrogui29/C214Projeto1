44509a2fcdfda23a3472a63bab109e6a5e4a5ad2
Lucas Couto <lucascouto04@hotmail.com>
2024-09-12 16:16:25 -0300

    Adicionando mais casos de testes

--- src/test/java/TesteBuscaHorario.java
+++ src/test/java/TesteBuscaHorario.java
@@ -53,7 +53,7 @@ public class TesteBuscaHorario {
     }
 
     @Test
-    public void testPredioCorreto_Sala1a5() {
+    public void testPredioCorreto_Sala1a5_Sucesso() {
         when(horarioServicoMock.buscaHorario(1)).thenReturn(
                 "{\"nomeDoProfessor\": \"Prof. Teste\", \"horarioDeAtendimento\": \"08:00 - 10:00\", \"periodo\": \"matutino\", \"sala\": \"4\", \"predio\": [1]}"
         );
@@ -64,7 +64,7 @@ public class TesteBuscaHorario {
     }
 
     @Test
-    public void testPredioCorreto_Sala6a10() {
+    public void testPredioCorreto_Sala6a10_Sucesso() {
         when(horarioServicoMock.buscaHorario(2)).thenReturn(
                 "{\"nomeDoProfessor\": \"Prof. Teste\", \"horarioDeAtendimento\": \"08:00 - 10:00\", \"periodo\": \"matutino\", \"sala\": \"7\", \"predio\": [2]}"
         );
@@ -75,7 +75,7 @@ public class TesteBuscaHorario {
     }
 
     @Test
-    public void testPredioCorreto_Sala11a15() {
+    public void testPredioCorreto_Sala11a15_Sucesso() {
         when(horarioServicoMock.buscaHorario(3)).thenReturn(
                 "{\"nomeDoProfessor\": \"Prof. Teste\", \"horarioDeAtendimento\": \"08:00 - 10:00\", \"periodo\": \"matutino\", \"sala\": \"12\", \"predio\": [3]}"
         );
@@ -86,7 +86,7 @@ public class TesteBuscaHorario {
     }
 
     @Test
-    public void testPredioCorreto_Sala16a20() {
+    public void testPredioCorreto_Sala16a20_Sucesso() {
         when(horarioServicoMock.buscaHorario(4)).thenReturn(
                 "{\"nomeDoProfessor\": \"Prof. Teste\", \"horarioDeAtendimento\": \"08:00 - 10:00\", \"periodo\": \"matutino\", \"sala\": \"18\", \"predio\": [4]}"
         );
@@ -97,7 +97,7 @@ public class TesteBuscaHorario {
     }
 
     @Test
-    public void testPredioCorreto_Sala21a25() {
+    public void testPredioCorreto_Sala21a25_Sucesso() {
         when(horarioServicoMock.buscaHorario(5)).thenReturn(
                 "{\"nomeDoProfessor\": \"Prof. Teste\", \"horarioDeAtendimento\": \"08:00 - 10:00\", \"periodo\": \"matutino\", \"sala\": \"22\", \"predio\": [6]}"
         );
@@ -116,5 +116,36 @@ public class TesteBuscaHorario {
         HorarioAtendimento horario = buscaHorario.buscaHorarioProfessor(6);
 
         assertEquals(0, horario.getPredio());
+    }
+
+    @Test
+    public void testMockBuscaProfessor_Sucesso() {
+        // Dado que o serviço retorna um JSON válido para o professor de ID 7
+        when(horarioServicoMock.buscaHorario(7)).thenReturn(
+                "{\"nomeDoProfessor\": \"Prof. Lucas\", \"horarioDeAtendimento\": \"19:00 - 21:00\", \"periodo\": \"noturno\", \"sala\": \"6\"}"
+        );
+
+        // Quando buscamos o professor de ID 7
+        HorarioAtendimento horario = buscaHorario.buscaHorarioProfessor(7);
+
+        // Então o nome do professor deve ser "Prof. Lucas"
+        assertEquals("Prof. Lucas", horario.getNomeDoProfessor());
+        assertEquals("19:00 - 21:00", horario.getHorarioDeAtendimento());
+        assertEquals("noturno", horario.getPeriodo());
+        assertEquals(6, horario.getSala());
+        assertEquals(2, horario.getPredio());
+    }
+
+    @Test
+    public void testBuscaHorarioProfessor_JSONComCamposAdicionais() {
+        when(horarioServicoMock.buscaHorario(10)).thenReturn(
+                "{\"nomeDoProfessor\": \"Prof. Teste\", \"horarioDeAtendimento\": \"08:00 - 10:00\", \"periodo\": \"matutino\", \"sala\": \"8\", \"predio\": [2], \"campoAdicional\": \"extra\"}"
+        );
+
+        HorarioAtendimento horario = buscaHorario.buscaHorarioProfessor(10);
+
+        // Teste para verificar se o método lida com JSON que contém campos adicionais não esperados
+        assertEquals("Prof. Teste", horario.getNomeDoProfessor());
+        assertEquals(2, horario.getPredio());
     }
 }
